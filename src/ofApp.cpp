a49158307578307445cf8f0be38da7971f33e2bc
Skyrain25 <shontellebarker@gmail.com>
2017-03-29 11:26:52 -0400

    Lab 8 Completed

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,10 +1,32 @@
 #include "ofApp.h"
+#include <random>
+
+using namespace std;
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
-X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
-Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
+void ofApp::setup()
+	{
+		jewel.load("diamond.png");// image to use (must be stored in "random/bin/data" folder)
+		float meanx =1000.0f, sdx = 15.0f, meany=800.0f, sdy=15.0f;
+		std::random_device rd;
+	
+		std::mt19937 en(rd());
+		
+		std::normal_distribution< >distX(meanx,sdx);
+		
+		for (int i=0; i<20; i++)
+		   
+		   X.push_back(distX(en));
+		   
+		   std::normal_distribution< >distY(meany,sdy);
+		
+		for (int i=0; i<20; i++)
+		   
+		   Y.push_back(distY(en));
+	 		   	      
+
+//X = {500, 1000, 30, 100, 300, 200, 750, 380 };// set of x-coordinates for jewel
+//Y = {630, 400, 900,40, 200, 0, 90, 30}; //set of y-coordinates for jewel
 }
 
 //--------------------------------------------------------------
